557cf045ef7087c0ea309353144018b5dab32485
jonatan777 <jonataneduardo777@gmail.com>
2023-03-21 21:47:14 -0300

    segundo commit

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -1,11 +1,13 @@
-var api = ' <div class="w3-panel w3-black"><p style="font-size: 12px;">APIs são mecanismos que permitem que dois componentes de software se comuniquem usando um conjunto de definições e protocolos. </p></div>';
+var institucional = ' <div class="w3-panel w3-black"><p style="font-size: 12px;">Um site institucional é o tipo mais comum encontrado pela web. O objetivo dele é apresentar a empresa e fazer marketing de conteúdo.</p></div>';
 var blog = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Blog é um site informativo, também chamado de diário online, onde os conteúdos são apresentados em ordem cronológica inversa, ou seja, com destaque para as publicações mais recentes. Diferente de um site institucional de uma empresa, onde os conteúdos são sempre os mesmos, o blog é uma página atualizada frequentemente.</p></div>';
 var e_commerce = '<div class="w3-panel w3-black"><p style="font-size: 12px;">E-commerce, ou comércio eletrônico, refere-se às vendas pela internet de produtos e serviços. Assim, todas as transações comerciais são realizadas por meio de ferramentas online.</p></div>';
 var lading_page = '<div class="w3-panel w3-black"><p style="font-size: 12px;">  Landing Page é uma página que tem como objetivo gerar conversão dos visitantes em leads, oportunidades, clientes ou vendas. Por isso é tão importante criar uma que traga, de fato, resultados.  </p></div>';
-var portfolio = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Um conjunto de páginas web, por conseguinte, forma um site da web. As páginas costumam estar reunidas sob um domínio comum para que o sítio em questão seja acessível a partir de uma mesma direção na Internet.</p></div>';
-var site = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Atualmente o site é a mídia digital mais barata para fazer marketing. Além de seu baixo custo de confecção, manter ele ativo é muito barato.</p></div>';
+var portfolio = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Um site de portfólio é uma forma única de mostrar seu trabalho e permitir que outras pessoas saibam mais sobre você. Um portfólio pode ser usado para atrair clientes, fechar negócios, candidatar a empregos e outros fins. </p></div>';
+var pwa = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Da forma mais simples, um PWA é uma página criada com o uso de tecnologias da web mas direcionada aos aplicativos móveis.</p></div>';
+var catalago = '<div class="w3-panel w3-black"><p style="font-size: 12px;">Para quem deseja ter um site com seus produtos e mostrá-los a seus clientes, como um catálogo digital.</p></div>';
 
 
+ 
 
 /**---------------------------API-------------------------------------------- */
 
@@ -13,10 +15,10 @@ var API = document.getElementById("1");
 API.onclick = function () { onClickApi() };
 
 function onClickApi() {
-  if (API.innerHTML == "API") {
-    return API.innerHTML = api;
-  } else if (API.innerHTML == api) {
-    return API.innerHTML = "API";
+  if (API.innerHTML == "INSTITUCIONAL") {
+    return API.innerHTML = institucional;
+  } else if (API.innerHTML == institucional) {
+    return API.innerHTML = "INSTITUCIONAL";
   }
 }
 
@@ -40,10 +42,10 @@ var E_COMMERCE = document.getElementById("3");
 E_COMMERCE.onclick = function () { onClickE_Commerce() };
 
 function onClickE_Commerce() {
-  if (E_COMMERCE.innerHTML == "E-COMMERCE") {
+  if (E_COMMERCE.innerHTML == "LOJA VIRTUAL") {
     E_COMMERCE.innerHTML = e_commerce;
   } else if (E_COMMERCE.innerHTML == e_commerce) {
-    return E_COMMERCE.innerHTML = "E-COMMERCE"
+    return E_COMMERCE.innerHTML = "LOJA VIRTUAL"
   }
 }
 
@@ -53,10 +55,10 @@ var PORFOLIO = document.getElementById("4");
 PORFOLIO.onclick = function () { onClickPortfolio() };
 
 function onClickPortfolio() {
-  if (PORFOLIO.innerHTML == "PORTFOLIO") {
+  if (PORFOLIO.innerHTML == "PORTFÓLIO") {
     return PORFOLIO.innerHTML = portfolio;
   } else if (PORFOLIO.innerHTML == portfolio) {
-    return PORFOLIO.innerHTML = "PORTFOLIO"
+    return PORFOLIO.innerHTML = "PORTFÓLIO"
   }
 }
 
@@ -67,10 +69,10 @@ var LADING_PAGE = document.getElementById("5");
 LADING_PAGE.onclick = function () { onClickLading_page() };
 
 function onClickLading_page() {
-  if (LADING_PAGE.innerHTML == "LADING PAGE") {
+  if (LADING_PAGE.innerHTML == "PAGINA UNICA") {
     return LADING_PAGE.innerHTML = lading_page;
   } else if (LADING_PAGE.innerHTML == lading_page) {
-    return LADING_PAGE.innerHTML = "LADING PAGE"
+    return LADING_PAGE.innerHTML = "PAGINA UNICA"
 
   }
 }
@@ -79,13 +81,28 @@ function onClickLading_page() {
 /**--------------------------SITE--------------------------------------------- */
 
 var SITE = document.getElementById("6");
-SITE.onclick = function () { onClickSite() };
+SITE.onclick = function () { onClickPwa() };
 
-function onClickSite() {
-  if (SITE.innerHTML == "SITE") {
-    return SITE.innerHTML = site
-  } else if (SITE.innerHTML == site) {
-    return SITE.innerHTML = "SITE"
+function onClickPwa() {
+  if (SITE.innerHTML == "PWA") {
+    return SITE.innerHTML = pwa
+  } else if (SITE.innerHTML == pwa) {
+    return SITE.innerHTML = "PWA"
+  }
+}
+
+
+
+/**-------------------------- catalágo --------------------------------------------- */
+
+var CATALAGO = document.getElementById("7");
+CATALAGO.onclick = function () { onClickCatalago() };
+
+function onClickCatalago() {
+  if (CATALAGO.innerHTML == "CATÁLAGO DIGITAL") {
+    return CATALAGO.innerHTML = catalago
+  } else if (CATALAGO.innerHTML == catalago) {
+    return CATALAGO.innerHTML = "CATÁLAGO DIGITAL"
   }
 }
 
